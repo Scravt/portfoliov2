75ea098b2f41ae179d182288115101c90c4675b8
Scravt <34680110ax@gmail.com>
2025-09-07 19:06:07 -0300

    Añadir sección Showcase con animaciones GSAP y referencias de proyectos

---
+++ src/sections/ShowcaseSection.jsx
@@ -0,0 +1,74 @@
+import React, { useRef } from 'react'
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const ShowcaseSection = () => {
+    const sectionRef = useRef(null);
+    const project1Ref = useRef(null);
+    const project2Ref = useRef(null);
+    const project3Ref = useRef(null);
+    const projects = [project1Ref.current, project2Ref.current, project3Ref.current];
+
+    projects.forEach((project, index) => {
+        gsap.fromTo(project,
+            { opacity: 0, y: 50 },
+            {
+                opacity: 1,
+                y: 0,
+                duration: 1,
+                delay: index * 0.3,
+                scrollTrigger: {
+                    trigger: project,
+                    start: "top bottom-=100",
+                    markers: true,
+                    toggleActions: "play none none none"
+                },
+            });
+
+    });
+    useGSAP(() => {
+        gsap.fromTo(sectionRef.current, { opacity: 0 }, { opacity: 1, duration: 1.5 })
+
+    }, []);
+
+    return (
+        <section id='work' ref={sectionRef} className='app-showcase '>
+            <div className='w-full'>
+                <div className='showcaselayout'>
+                    <div className='first-project-wrapper' ref={project1Ref}>
+                        <div className='image-wrapper'>
+                            <img src="/public/images/project1.png" alt="Ryde" />
+                        </div>
+                        <div className='text-content'>
+                            <h2>
+                                On-demand Rides Made Simple whith Powerful, User-Friendly App called Ryde
+                            </h2>
+                            <p>
+                                An app built with React Native, Redux, Node.js, Express, for  fast user Friendly experience.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='project-list-wrapper overflow-hidden'>
+                        <div className='project' ref={project2Ref}>
+                            <div className='image-wrapper bg-[#ffefdb]'>
+                                <img src="/public/images/project2.png" alt="library" />
+                            </div>
+                            <h2>Library Platform</h2>
+                        </div>
+                        <div className='project' ref={project3Ref}>
+                            <div className='image-wrapper bg-[#ffe7eb]'>
+                                <img src="/public/images/project3.png" alt="YC Directory" />
+                            </div>
+                            <h2>YC Directory</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ShowcaseSection
