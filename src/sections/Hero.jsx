da6964e5f21d12e4a7923e2594c3b9a475db1fa7
Scravt <34680110ax@gmail.com>
2025-09-09 15:14:30 -0300

    Añadir componente NavBar con manejo de scroll y enlaces de navegación

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -29,43 +29,45 @@ const Hero = () => {
 
             <div className='hero-layout'>
                 {/* Left side hero content */}
-                <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
-                    <div className='flex flex-col gap-7'>
-                        <div className='hero-text'>
-                            <h1>Shaping
-                                <span className='slide'>
-                                    <span className='wrapper'>
-                                        {words.map((word, index) => (
-                                            <span key={index} className='flex items-center md:gap-3 gap-1 pb-2'>
+                <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word, index) => (
+                      <span
+                        key={index}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt="person"
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1>
+            </div>
 
-                                                <img
-                                                    src={word.imgPath}
-                                                    alt={word.text}
-                                                    className='xl:size-12 md:size-10 sm:size-7 md:p-2 p-1 rounded-full bg-white-50' />
-                                                <span>{word.text}</span>
-                                            </span>
-                                        ))}
-                                    </span>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi, I’m Adrian, a developer based in Croatia with a passion for
+              code.
+            </p>
 
-                                </span>
-
-                            </h1>
-
-                            <h1>into real Projects</h1>
-                            <h1>that deliver results</h1>
-                            <p className='text-white-50 md:text-xl relative z-10 pointer-events-none'>
-                                Hello, I'm Gonzalo
-                            </p>
-                            <Button
-                                className='md:w-80 md:h-167 w-60 h-12'
-                                
-                                text='See my Work'
-                                id={'counter'}
-                            />
-
-                        </div>
-                    </div>
-                </header>
+            <Button
+              text="See My Work"
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="counter"
+            />
+          </div>
+        </header>
                 <figure>
                     <div className='hero-3d-layout'>
                         <HeroExpierience isTablet={isTablet} />
