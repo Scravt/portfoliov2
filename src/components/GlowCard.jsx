980cc537f06cb419cd11e89a0fc9db409f29913b
Scravt <34680110ax@gmail.com>
2025-09-09 19:02:59 -0300

    Añadir componente GlowCard y mejorar la sección Experience con tarjetas interactivas

---
+++ src/components/GlowCard.jsx
@@ -0,0 +1,42 @@
+import {useRef} from 'react'
+
+const GlowCard = ({card, children, index}) => {
+    const cardRef = useRef([]);
+
+    const handleMouseMove = (index) =>(e) => {
+        const card = cardRef.current[index];
+        if(!card)return;
+    
+    //Get mouse position relative to the card
+    const rect = card.getBoundingClientRect();
+    const mouseX = e.clientX - rect.left - rect.width / 2;
+    const mouseY = e.clientY - rect.top - rect.height / 2;
+   
+    //angulo del centro de la card
+    let angle =Math.atan2(mouseY, mouseX) * (180 / Math.PI) + 90;
+    angle = (angle + 360) % 360; 
+
+    card.style.setProperty('--start', angle + 60 );
+    }
+
+  return (
+    <div ref={(el)=>(cardRef.current[index]=el)} 
+         onMouseMove={handleMouseMove(index)}
+         className='card card-border timeline-card rounded-xl p-10'>
+        <div className='glow'/>
+        <div className='flex items-center gap-1 mb-5'>
+            {Array.from({length:5},(_,i) => (
+                <img key={i} src={`/images/star.png`} alt={`Star`} className='size-5'/>
+            ))}
+        </div>
+        <div className='mb-5'>
+            <p className='text-white-50 text-lg'>
+                {card.review}
+            </p>
+        </div>
+        {children}
+    </div>
+  )
+}
+
+export default GlowCard
